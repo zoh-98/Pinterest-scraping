2b0ac5267d29280be12722209b73510245176495
Gry KJ <106838880+zoh-98@users.noreply.github.com>
2024-01-24 01:51:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.get('/', async (req, res) =>{
     }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || port, () => {
+  console.log(`Server is running on port ${process.env.PORT || port}`);
 });
